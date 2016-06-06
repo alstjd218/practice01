3cdb7562bfffd904149f1404babefde3371fcc12
alstjd218 <alstjd218@hanyang.ac.kr>
2016-06-06 11:15:27 +0900

    Add

---
+++ Tower of Hanoi_final.py
@@ -0,0 +1,25 @@
+def moves(a,b,c,num):
+    if num==1:
+        print a,c
+    else:
+        moves(a,c,b,num-1)
+        print a,c
+        moves(b,a,c,num-1)
+        
+        
+        
+def countOfMoves(num):
+    count=0
+    for k in range(num):
+        count+=count+1
+    return count
+
+
+numOfDisk=int(raw_input('Input the number of disks : '))
+print countOfMoves(numOfDisk)
+
+a,b,c=1,2,3
+
+if numOfDisk<20:
+     moves(a,b,c,numOfDisk)
+
